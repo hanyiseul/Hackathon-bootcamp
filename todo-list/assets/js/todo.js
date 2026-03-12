c2e0cd7ef7569fc9c969369265b4d604a7d43d80
hanyiseul <seulli_hys@daum.net>
2026-03-12 17:39:06 +0900

    1차 해커톤 제출

---
+++ todo-list/assets/js/todo.js
@@ -0,0 +1,27 @@
+const todo = [
+  {
+    id: "todo0",
+    content: "투두 리스트 UI 만들기",
+    completed : false,
+  },
+  {
+    id: "todo1",
+    content: "할 일 추가 기능 구현",
+    completed : false,
+  },
+  {
+    id: "todo2",
+    content: "완료 체크 기능 만들기",
+    completed : false,
+  },
+  {
+    id: "todo3",
+    content: "로컬스토리지 저장 기능 추가",
+    completed : false,
+  },
+  {
+    id: "todo4",
+    content: "반응형 스타일 적용",
+    completed : false,
+  }
+];
