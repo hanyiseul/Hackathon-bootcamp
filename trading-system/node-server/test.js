9adcdf67c5f57faab550f015b89c8b32470db3c0
hanyiseul <seulli_hys@daum.net>
2026-04-24 23:59:53 +0900

    테스트코드 주석

--- trading-system/node-server/test.js
+++ trading-system/node-server/test.js
@@ -1,32 +1,32 @@
 // 회원가입 테스트코드
-(async () => {
-  const res = await fetch("http://localhost:3000/api/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      user_id: "test12345",
-      password: "1234",
-    }),
-  });
+// (async () => {
+//   const res = await fetch("http://localhost:3000/api/signup", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       user_id: "test12345",
+//       password: "1234",
+//     }),
+//   });
 
-  const data = await res.json();
-  console.log("회원가입", data);
-})();
+//   const data = await res.json();
+//   console.log("회원가입", data);
+// })();
 
 // 로그인 테스트코드
-(async () => {
-  const res = await fetch("http://localhost:3000/api/login", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      user_id: "test1234",
-      password: "1234",
-    }),
-  });
-  const data = await res.json();
-  console.log("로그인", data);
-})();
+// (async () => {
+//   const res = await fetch("http://localhost:3000/api/login", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       user_id: "test1234",
+//       password: "1234",
+//     }),
+//   });
+//   const data = await res.json();
+//   console.log("로그인", data);
+// })();
